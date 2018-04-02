d8eff3cb5f099164213290f6acd0bd8bf2715d18
sudhir meena <sureshmeena512@gmail.com>
2018-04-02 16:20:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export var firebaseConfig = {
+  apiKey: "AIzaSyAtYC3nCh0tmwr5JUZXX3RNm91m0mmEBzo",
+  authDomain: "chat-app-867b6.firebaseapp.com",
+  databaseURL: "https://chat-app-867b6.firebaseio.com",
+  projectId: "chat-app-867b6",
+  storageBucket: "chat-app-867b6.appspot.com",
+  messagingSenderId: "819134768797"
+};
+
 
 
 @NgModule({
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
